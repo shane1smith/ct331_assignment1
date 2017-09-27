628666759d73da9ded9069c2354fafa45587b4e6
shane1smith <31901318+shane1smith@users.noreply.github.com>
2017-09-27 16:46:36 +0100

    Add files via upload

---
+++ src/q1/Question1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+//Function prototypes
+void createInt();
+void createIntPointer();
+void createLong();
+void createDoublePointer();
+void createCharPointerPointer();
+
+void createInt(){
+	int i = 3;
+	printf(i)
+	printf(sizeof(i));
+}
+
+void createIntPointer(){
+	int i = 3;
+	int* p = &i;
+	printf(p);
+	printf(sizeof(p));
+}
+
+void createLong(){
+	long l = 
+}
+
+/*Main is the entry point to the programme
+There are 2 arguments. arg is the number of
+command line arguments, argv is the arguments.*/ 
+void main(int arg,char* argv[] ){
+	
+}
