9ccabf2aaaf51edc02a29fc72c5643afda5e015b
shane1smith <31901318+shane1smith@users.noreply.github.com>
2017-10-01 13:02:44 +0100

    Update assignment.c

--- src/q2/assignment.c
+++ src/q2/assignment.c
@@ -3,6 +3,29 @@
 #include "tests.h"
 
 int main(int arg, char* argc[]){
+  //Declare 3 listElement pointers: head, middle and tail.
+  struct listElement *head,*middle,*tail = NULL;
+  
+  size_t size = sizeof(char);
+  //Create elements
+  head = createEl('a',size);
+  middle = createEl('b',size);
+  tail = createEl('c',size);
+  //Link the elements in the list together with pointers
+  //to make a linked list
+  head->next = middle;
+  middle->next = tail;
+  tail->next = NULL;
+  
+  //Use traverse function to see that the elements were all created
+  traverse(head);
+  
+  //Test out the length function
+  int length = length(head);
+  printf("The length of the list is %d",length);
+  
+  
+  
   
   
   return 0;
